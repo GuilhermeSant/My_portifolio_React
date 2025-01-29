c989de36d35abba941b1e886323462544ba67e23
Guilherme <guilherme_vinicius14@live.com>
2025-01-29 18:08:31 -0300

    update skills and begin to implement portifolio web

---
+++ my-portifolio/src/components/PortifolioWeb/PortifolioWeb-data.js
@@ -0,0 +1,27 @@
+import imagemJaneDoe from '../../img/jane_doe.png';
+import imagemNetflixClone from '../../img/netflix-clone.png';
+import imagemDisneyClone from '../../img/disney-clone.png';
+
+export const PortifolioWebData = [
+  {
+    title: "Jane Doe Site - HTML | CSS",
+    img: imagemJaneDoe,
+    alt: "Site Jane-Doe",
+    description: "Site com home e links funcionais e página responsiva, com design moderno e intuitivo.",
+    github: "https://github.com/GuilhermeSant/Jane-Doe-Git"
+  },
+  {
+    title: "Netflix clone HTML | CSS",
+    img: imagemNetflixClone,
+    alt: "Site Netflix-clone",
+    description: "Site feito no curso de front-end da Digital House replicando a interface da Netflix.",
+    github: "https://github.com/GuilhermeSant/Netflix-clone"
+  },
+  {
+    title: "Disney Plus clone HTML | CSS",
+    img: imagemDisneyClone,
+    alt: "Site Disneyplus-clone",
+    description: "Projeto de clone do site Disney Plus feito na Digital House, com foco na responsividade.",
+    github: "https://github.com/GuilhermeSant/Netflix-clone"
+  },
+];
